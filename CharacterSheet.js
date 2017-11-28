fd5d1e67f9d60ad50f27e584d2afdec4b309a0f4
Yves Gurcan <gurcan.yves+github@gmail.com>
2017-11-27 21:37:29 -0800

    Update CharacterSheet.js

--- CharacterSheet.js
+++ CharacterSheet.js
@@ -1,11 +1,50 @@
 
 class Input extends Component {
+    onChange = (input) => {
+        if (!this.props.onChange) return false
+        this.props.onChange(input.target)
+    }
+    render() {
+        return (
+            <Text>
+                <input name={this.props.name} value={this.props.value} type={this.props.type} onChange={this.onChange}/>
+            </Text>
+        )
+    }
+}
+
+class Label extends Component {
+    render() {
+        return (
+            <Text>
+                <label>{this.props.children}</label>
+            </Text>
+        )
+    }
+}
+
+class Group extends Component {
     render() {
         return (
             <View>
-                <input />
+                <Label>{this.props.label}</Label>
+                <Input name={this.props.name} value={this.props.value} type={this.props.type} onChange={this.props.on Change} />
             </View>
         )
     }
 }
 
+
+
+class RandomNumber extends Component {
+    generateNumber = () => {
+    }
+    render() {
+        return (
+            <View>
+                <Label>Number: {this.state.number}</Label>
+                <Button onClick={this.generateNumber}>Random Number</Button>
+            </View>
+        )
+    }
+}
