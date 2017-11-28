3446f40fca56b7c1887c260a5a520e8530a36d17
Yves Gurcan <gurcan.yves+github@gmail.com>
2017-11-27 22:06:09 -0800

    Update CharacterSheet.js

--- CharacterSheet.js
+++ CharacterSheet.js
@@ -34,10 +34,34 @@ class Group extends Component {
     }
 }
 
-
+class Button extends Component {
+    onClick = (input) => {
+        if (!this.props.onClick) return false
+        this.props.onClick(input.target)
+    }
+    render() {
+        return (
+            <button onClick={this.onClick}>{this.props.children}</button>
+        )
+    }
+}
 
 class RandomNumber extends Component {
     generateNumber = () => {
+        let randomizer = [
+            1,5,7,3,6,9,0,1,7,9,
+            3,9,2,8,1,7,4,9,7,8,
+            6,1,0,7,3,0,5,4,6,7,
+            0,2,8,9,2,9,6,0,2,4,
+            5,9,6,4,8,2,8,5,6,3,
+            0,3,1,3,9,7	5,0,1,5,
+            5,8,2,5,1,3,6,4,3,9,
+            7,0,4,8,6,4,5,1,4,2,
+            4,6,8,3,2,0,1,7,2,5,
+            8,3,7,0,9,6,2,4,8,1,
+        ]
+        let random = Math.floor(Math.random() * randomizer.length)
+        this.setState({number: randomizer[random]})
     }
     render() {
         return (
