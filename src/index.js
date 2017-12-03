40728563cbc10193be82778a38a170d844a4a26e
yvesgurcan <owner@AsteroideB612.domain>
2017-12-02 20:07:52 -0800

    service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './CharacterSheet'
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
