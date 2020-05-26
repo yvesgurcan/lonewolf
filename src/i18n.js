b1edf6a569d0619b131e69c547a21dd4b862b74d
Vargash <r.vittad@gmail.com>
2020-05-26 21:15:57 +0200

    Add gh-pages deployment target

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,19 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
  
 import Backend from 'i18next-xhr-backend';
- 
+
+import translationIT from "./locales/it/translation.json";
+import translationEN from "./locales/en/translation.json";
+
+const resources = {
+ en: {
+   translation: translationEN,
+ },
+ it: {
+   translation: translationIT,
+ },
+};
+
 i18n
   // learn more: https://github.com/i18next/i18next-xhr-backend
   .use(Backend)
@@ -15,6 +27,7 @@ i18n
     lng: 'it',
     fallbackLng: 'en',
     whitelist: ['en', 'it'],
+    resources,
  
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
