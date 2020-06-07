c0eab6baafd6203c10319d9c731bfff7d2e0d17a
yvesgurcan <gurcan.yves+github@gmail.com>
2020-06-07 15:38:43 -0700

    Add French translation

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -3,12 +3,16 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-xhr-backend';
 
-import translationIT from '../locales/it/translation.json';
 import translationEN from '../locales/en/translation.json';
+import translationFR from '../locales/fr/translation.json';
+import translationIT from '../locales/it/translation.json';
 
 const resources = {
     en: {
         translation: translationEN,
+    },
+    fr: {
+        translation: translationFR,
     },
     it: {
         translation: translationIT,
@@ -25,7 +29,7 @@ i18n
         debug: false,
         lng: 'en',
         fallbackLng: 'en',
-        whitelist: ['en', 'it'],
+        whitelist: ['en', 'fr', 'it'],
         resources,
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
