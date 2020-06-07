6af63eba8f133fed479f37d58d279808c8070df6
yvesgurcan <gurcan.yves+github@gmail.com>
2020-06-07 15:49:54 -0700

    Add some SEO

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Suspense, lazy, useEffect } from 'react';
+import React, { Fragment, Suspense, lazy, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
+import { Helmet } from 'react-helmet';
 import { store } from './store/store';
 
 const CharacterSheet = lazy(() => import('./views/CharacterSheet'));
@@ -14,12 +15,19 @@ export default () => {
             const { CharacterSheet } = JSON.parse(storedState);
             i18next.changeLanguage(CharacterSheet.Language);
         }
-    }, [])
+    }, []);
     return (
-        <Provider store={store}>
-            <Suspense fallback={t('Loading')}>
-                <CharacterSheet />
-            </Suspense>
-        </Provider>
+        <Fragment>
+            <Helmet>
+                <html lang={t('locale')} />
+                <title>{t('CharacterSheet')}</title>
+                <meta name="description" content={t('Site description')} />
+            </Helmet>
+            <Provider store={store}>
+                <Suspense fallback={t('Loading')}>
+                    <CharacterSheet />
+                </Suspense>
+            </Provider>
+        </Fragment>
     );
 };
